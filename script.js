6b81793651471fa97a98cd56973f7335c968b103
Nisha_Kala <117833327+Nishakala@users.noreply.github.com>
2023-10-14 16:38:53 +0530

    Add files via upload
    📋 Unique To-Do List 📝
    
    Elevate your task management experience with this web-based to-do list. Crafted with HTML, CSS, and JavaScript, it seamlessly combines functionality and aesthetics in a sub-250-word package.
    
    ✅ Key Features:
    
    📥 Add Tasks: Input tasks and enjoy a beautifully designed list.
    🗑️ Effortless Deletion: Remove completed tasks with ease.
    💼 Task Organization: Keep your to-dos tidy and prioritize effectively.
    🎨 Stylish Design: A unique and visually appealing layout.
    🚀 User-Friendly: Simple and intuitive, catering to all users.
    🌟 Visual Appeal: A balance of form and function, making tasks a delight.
    This project flaunts front-end proficiency, empowering users to manage tasks stylishly. Stay on top of your to-do list, set priorities, and streamline your workflow. It's the perfect fusion of form and function - experience it with the Unique To-Do List. 📝✨🚀

---
+++ script.js
@@ -0,0 +1,20 @@
+const taskInput = document.getElementById('task');
+const taskList = document.getElementById('taskList');
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span>${taskText}</span>
+            <button class="delete-btn" onclick="deleteTask(this)">Delete</button>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+}
+
+function deleteTask(button) {
+    const li = button.parentElement;
+    li.remove();
+}
